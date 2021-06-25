2a7f441dcf30a8075bb3070ccf194eb908fd2370
NOUHOUM TAMBOURA <ntamboura@groupeisi.com>
2021-06-25 19:42:26 +0000

    init commit

---
+++ src/main/java/com/slaen/salen/SalenApplication.java
@@ -0,0 +1,13 @@
+package com.slaen.salen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalenApplication.class, args);
+	}
+
+}
